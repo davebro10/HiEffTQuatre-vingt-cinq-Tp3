34404fc7a11ddd0d9f58916bce80e3759a292596
vile2401 <vile2401@USherbrooke.ca>
2019-04-07 17:22:59 -0400

    It will now be possible to open an ApplicationPanel within the Designer View in Visual Studio

--- client/client/client/ApplicationPanel.cs
+++ client/client/client/ApplicationPanel.cs
@@ -1,9 +1,14 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using serveur.Models;
 
 namespace client
 {
+#if DEBUG
+    public class ApplicationPanel : UserControl, ISynchronizable
+#else
     public abstract class ApplicationPanel : UserControl, ISynchronizable
+#endif
     {
         private readonly MainForm _parent;
 
@@ -12,12 +17,25 @@ namespace client
             _parent = parent;
         }
 
+#if DEBUG
+        [Obsolete("Designer Only", true)]
+        public ApplicationPanel() { }
+
+        public virtual void Synchronize() { }
+#else
         public abstract void Synchronize();
+#endif
 
         protected Client ActiveClient
         {
             get => _parent.ActiveClient;
             set => _parent.ActiveClient = value;
+        }
+
+        protected Groupe ActiveGroup
+        {
+            get => _parent.ActiveGroup;
+            set => _parent.ActiveGroup = value;
         }
 
         protected void ChangeActivePanel(MainForm.Panel panel) => _parent.CurrentPanel = panel;
